a0f024f7f86573e8378ebc4ff3b1208f3127cc01
Jer Miller <jeremie.miller@gmail.com>
2025-07-14 22:01:22 -0600

    feat: implement sunstone CLI commands and update script entry point

--- think/__init__.py
+++ think/__init__.py
@@ -2,10 +2,12 @@ from .border_detect import detect_border
 from .cluster import cluster as cluster_day
 from .cluster import cluster_range
 from .entities import Entities
+from .sunstone import main as sunstone_main
 
 __all__ = [
     "cluster_day",
     "cluster_range",
-    "Entities",
+    "Entities", 
     "detect_border",
+    "sunstone_main",
 ]
