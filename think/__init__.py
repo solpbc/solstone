66d29f321161854881ce8a477ee756460f0e4983
Jer Miller <jeremie.miller@gmail.com>
2025-07-23 08:55:19 -0600

    Remove agent_instructions from exports in __init__.py

--- think/__init__.py
+++ think/__init__.py
@@ -5,17 +5,10 @@ from .entities import Entities
 from .sunstone import main as sunstone_main
 
 
-def agent_instructions():
-    from .agent import agent_instructions as _agent_instructions
-
-    return _agent_instructions()
-
-
 __all__ = [
     "cluster_day",
     "cluster_range",
     "Entities",
     "detect_border",
     "sunstone_main",
-    "agent_instructions",
 ]
