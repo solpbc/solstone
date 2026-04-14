5f417063616a68ff433658643b31e3c5760f8677
Jer Miller <jeremie.miller@gmail.com>
2026-04-14 13:58:52 -0600

    Update search_ping_identity

--- search_ping_identity.py
+++ search_ping_identity.py
@@ -3,7 +3,7 @@ import json
 
 # The user's query parameters
 query = "" # Optional: add a keyword search
-day = "20260407"
+day = "20260414"
 facet = "ping_identity"
 
 total_hits, results = search_journal(query, day=day, facet=facet, limit=100)
